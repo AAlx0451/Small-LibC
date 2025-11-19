6e57c0029295bfcb540aae34b7055b8fc52dc61f
Alexander <aalx2176@gmail.com>
2025-11-20 01:45:11 +0300

    utf8 support in stdlib

--- src/c/stdlib/mbstowcs.c
+++ src/c/stdlib/mbstowcs.c
@@ -1,25 +1,40 @@
 #include <stdlib.h>
 
 size_t mbstowcs(wchar_t *pwcs, const char *s, size_t n) {
-	size_t i = 0;
-	
-	if (pwcs == NULL) {
-		while (*s != '\0') {
-			i++;
-			s++;
-		}
-		return i;
-	}
+    size_t count = 0;
+    int len;
 
-	while (i < n) {
-		if (*s == '\0') {
-			pwcs[i] = 0;
-			return i;
-		}
-		pwcs[i] = (wchar_t)*s;
-		s++;
-		i++;
-	}
-	
-	return n;
+    if (pwcs == NULL) {
+        while (*s) {
+            len = mblen(s, MB_CUR_MAX * 4);
+            if (len == -1) {
+                return (size_t)-1;
+            }
+            if (len == 0) {
+                break;
+            }
+            s += len;
+            count++;
+        }
+        return count;
+    }
+    while (count < n) {
+        wchar_t wc;
+        len = mbtowc(&wc, s, MB_CUR_MAX * 4);
+        
+        if (len == -1) {
+            return (size_t)-1;
+        }
+        
+        if (len == 0) {
+            *pwcs = L'\0';
+            return count;
+        }
+
+        *pwcs++ = wc;
+        s += len;
+        count++;
+    }
+
+    return count;
 }
--- src/c/stdlib/wcstombs.c
+++ src/c/stdlib/wcstombs.c
@@ -1,20 +1,47 @@
 #include <stdlib.h>
 
 size_t wcstombs(char *s, const wchar_t *pwcs, size_t n) {
-	size_t i = 0;
+    size_t bytes_written = 0;
+    int len;
 
-	while (i < n) {
-		if (*pwcs == 0) {
-			s[i] = '\0';
-			return i;
-		}
-		s[i] = (char)*pwcs;
-		pwcs++;
-		i++;
-	}
+    if (s == NULL) {
+        while (*pwcs) {
+            char temp[MB_CUR_MAX * 4];
+            len = wctomb(temp, *pwcs++);
+            if (len == -1) {
+                return (size_t)-1;
+            }
+            bytes_written += len;
+        }
+        return bytes_written;
+    }
+    
+    while (bytes_written < n && *pwcs) {
+        char temp_buf[MB_CUR_MAX * 4];
+        len = wctomb(temp_buf, *pwcs);
 
-	if (n > 0) {
-	}
+        if (len == -1) {
+            return (size_t)-1;
+        }
 
-	return n;
+        if (bytes_written + len > n) {
+            break;
+        }
+
+        {
+            int i;
+            for (i = 0; i < len; ++i) {
+                s[bytes_written + i] = temp_buf[i];
+            }
+        }
+        
+        bytes_written += len;
+        pwcs++;
+    }
+
+    if (bytes_written < n) {
+        s[bytes_written] = '\0';
+    }
+
+    return bytes_written;
 }
