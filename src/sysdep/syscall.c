15023517cf4a4c1a01b3d03a56f081f6ce83cfad
Polina <aalx2176@gmail.com>
2025-11-27 15:33:02 +0300

    syscall0-6, string.h

--- src/sysdep/syscall.c
+++ src/sysdep/syscall.c
@@ -48,6 +48,193 @@ long syscall(long number, ...) /* we use long, which is redundant */
     return r0;
 }
 
+long syscall0(long number)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = 0; /* Dummy init for output */
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"             /* Execute syscall */
+        "mov %[err], #0\n\t"        /* Reset error flag */
+        "movcs %[err], #1\n\t"      /* Set error flag if Carry Bit is set */
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r12)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall1(long number, long arg1)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r12)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall2(long number, long arg1, long arg2)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r1 __asm__("r1") = arg2;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r12)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall3(long number, long arg1, long arg2, long arg3)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r1 __asm__("r1") = arg2;
+    register long r2 __asm__("r2") = arg3;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r2), "r" (r12)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall4(long number, long arg1, long arg2, long arg3, long arg4)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r1 __asm__("r1") = arg2;
+    register long r2 __asm__("r2") = arg3;
+    register long r3 __asm__("r3") = arg4;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r1 __asm__("r1") = arg2;
+    register long r2 __asm__("r2") = arg3;
+    register long r3 __asm__("r3") = arg4;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "sub sp, sp, #4\n\t"        /* Allocate stack for arg5 */
+        "str %[a5], [sp]\n\t"       /* Store arg5 */
+        "svc #0x80\n\t"
+        "add sp, sp, #4\n\t"        /* Restore stack */
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), [a5] "r" (arg5)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
+long syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = arg1;
+    register long r1 __asm__("r1") = arg2;
+    register long r2 __asm__("r2") = arg3;
+    register long r3 __asm__("r3") = arg4;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "sub sp, sp, #8\n\t"        /* Allocate stack for arg5, arg6 */
+        "str %[a5], [sp]\n\t"       /* Store arg5 */
+        "str %[a6], [sp, #4]\n\t"   /* Store arg6 */
+        "svc #0x80\n\t"
+        "add sp, sp, #8\n\t"        /* Restore stack */
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), [a5] "r" (arg5), [a6] "r" (arg6)
+        : "memory", "cc"
+    );
+
+    if (error_flag)
+    {
+        errno = (int)r0;
+        return -1;
+    }
+
+    return r0;
+}
+
 # endif
 # ifdef __linux__
 #  error Linux not supported now
