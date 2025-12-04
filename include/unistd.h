3ea0e8d9efd7da047a4fa668f90421d65b136992
Polina <aalx2176@gmail.com>
2025-12-04 15:49:09 +0300

    critical bugfix in getpid, NO_SLIBC cppflag for portage

--- include/unistd.h
+++ include/unistd.h
@@ -9,6 +9,7 @@ typedef long ssize_t;
 
 /* FUNCTIONS */
 long syscall(long number, ...);
+# ifndef NO_SLIBC
 long syscall0(long number);
 long syscall1(long number, long arg1);
 long syscall2(long number, long arg1, long arg2);
@@ -17,6 +18,16 @@ long syscall4(long number, long arg1, long arg2, long arg3, long arg4);
 long syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5);
 long syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6);
 long syscall8(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7, long arg8);
+# else
+#  define syscall0 syscall
+#  define syscall1 syscall
+#  define syscall2 syscall
+#  define syscall3 syscall
+#  define syscall4 syscall
+#  define syscall5 syscall
+#  define syscall6 syscall
+#  define syscall8 syscall
+# endif
 
 ssize_t write(int fd, const void *buf, size_t count);
 pid_t fork(void);
