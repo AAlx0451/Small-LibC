5f8e78e71a8af409841375dfd4d1609619079cec
Polina <aalx2176@gmail.com>
2025-12-07 13:07:53 +0300

    one more syscall refactor, sbrk(), brk()

--- include/unistd.h
+++ include/unistd.h
@@ -9,7 +9,6 @@ typedef long ssize_t;
 
 /* FUNCTIONS */
 long syscall(long number, ...);
-# ifndef NO_SLIBC
 long syscall0(long number);
 long syscall1(long number, long arg1);
 long syscall2(long number, long arg1, long arg2);
@@ -18,16 +17,6 @@ long syscall4(long number, long arg1, long arg2, long arg3, long arg4);
 long syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5);
 long syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6);
 long syscall8(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7, long arg8);
-# else
-#  define syscall0 syscall
-#  define syscall1 syscall
-#  define syscall2 syscall
-#  define syscall3 syscall
-#  define syscall4 syscall
-#  define syscall5 syscall
-#  define syscall6 syscall
-#  define syscall8 syscall
-# endif
 
 ssize_t write(int fd, const void *buf, size_t count);
 pid_t fork(void);
@@ -39,5 +28,7 @@ int execve(const char *pathname, char *const argv[], char *const envp[]);
 off_t lseek(int fd, off_t offset, int whence);
 unsigned int sleep(unsigned int seconds);
 unsigned int usleep(unsigned int milliseconds);
+void *brk(void *x);
+void *sbrk(intptr_t size);
 
 #endif
--- src/sysdep/syscall.c
+++ src/sysdep/syscall.c
@@ -1,16 +1,46 @@
+/*
+ * SAFE syscall() implementation
+ * uses push and pop, which
+ * are safer for clang
+ * (add/str causes stack corruption
+ */
+
+/* this implementation is SAFE!!! */
+
 #include <stdarg.h>
 #include <errno.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+# define FORCE_ARM __attribute__((target("arm")))
+#else
+# define FORCE_ARM
+#endif
 
 #ifdef __arm__
 # ifdef __APPLE__
 
-long syscall(long number, ...) /* we use long, which is redundant */
+#define ASM_SYSCALL_TAIL(stack_bytes, restore_regs) \
+    "svc #0x80\n\t" \
+    "add sp, sp, %0\n\t" \
+    "mov %[err], #0\n\t" \
+    "movcs %[err], #1\n\t" \
+    restore_regs \
+    : "+r" (r0), [err] "=r" (error_flag)
+
+#define RETURN_RESULT \
+    if (error_flag) { \
+        errno = (int)r0; \
+        return -1; \
+    } \
+    return r0;
+
+FORCE_ARM
+long syscall(long number, ...)
 {
     va_list args;
     long arg1, arg2, arg3, arg4, arg5, arg6;
     long error_flag;
 
-    /* extract args and pin them to registers */
     va_start(args, number);
     arg1 = va_arg(args, long);
     arg2 = va_arg(args, long);
@@ -19,59 +49,47 @@ long syscall(long number, ...) /* we use long, which is redundant */
     arg5 = va_arg(args, long);
     arg6 = va_arg(args, long);
     va_end(args);
+
     register long r0 __asm__("r0") = arg1;
     register long r1 __asm__("r1") = arg2;
     register long r2 __asm__("r2") = arg3;
     register long r3 __asm__("r3") = arg4;
-    register long r4 __asm__("r4") = arg5;
-    register long r5 __asm__("r5") = arg6;
-    register long r12 __asm__("r12") = number; /* syscall number register */
-
-    __asm__ volatile (
-        "push {r4, r5, r6, r7}\n\t"
-        "svc #0x80\n\t"
-        "mov %[err], #0\n\t"
-        "movcs %[err], #1\n\t"
-        "pop {r4, r5, r6, r7}\n\t"
-        : "+r" (r0), [err] "=r" (error_flag)
-        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5)
-        : "memory", "cc", "r6", "r7"
-    );
-
-    // Error handling
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
-}
-
-long syscall0(long number)
-{
-    long error_flag;
-    register long r0 __asm__("r0") = 0; /* Dummy init for output */
     register long r12 __asm__("r12") = number;
 
     __asm__ volatile (
-        "svc #0x80\n\t"             /* Execute syscall */
-        "mov %[err], #0\n\t"        /* Reset error flag */
-        "movcs %[err], #1\n\t"      /* Set error flag if Carry Bit is set */
+        "push {%[a6]}\n\t"
+        "push {%[a5]}\n\t"
+        "svc #0x80\n\t"
+        "pop {r1, r2}\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
+        : "+r" (r0), [err] "=r" (error_flag)
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12),
+          [a5] "r" (arg5), [a6] "r" (arg6)
+        : "memory", "cc"
+    );
+    RETURN_RESULT
+}
+
+FORCE_ARM
+long syscall0(long number)
+{
+    long error_flag;
+    register long r0 __asm__("r0") = 0;
+    register long r12 __asm__("r12") = number;
+
+    __asm__ volatile (
+        "svc #0x80\n\t"
+        "mov %[err], #0\n\t"
+        "movcs %[err], #1\n\t"
         : "+r" (r0), [err] "=r" (error_flag)
         : "r" (r12)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall1(long number, long arg1)
 {
     long error_flag;
@@ -86,16 +104,10 @@ long syscall1(long number, long arg1)
         : "r" (r12)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall2(long number, long arg1, long arg2)
 {
     long error_flag;
@@ -111,16 +123,10 @@ long syscall2(long number, long arg1, long arg2)
         : "r" (r1), "r" (r12)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall3(long number, long arg1, long arg2, long arg3)
 {
     long error_flag;
@@ -137,16 +143,10 @@ long syscall3(long number, long arg1, long arg2, long arg3)
         : "r" (r1), "r" (r2), "r" (r12)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall4(long number, long arg1, long arg2, long arg3, long arg4)
 {
     long error_flag;
@@ -164,16 +164,10 @@ long syscall4(long number, long arg1, long arg2, long arg3, long arg4)
         : "r" (r1), "r" (r2), "r" (r3), "r" (r12)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5)
 {
     long error_flag;
@@ -181,29 +175,22 @@ long syscall5(long number, long arg1, long arg2, long arg3, long arg4, long arg5
     register long r1 __asm__("r1") = arg2;
     register long r2 __asm__("r2") = arg3;
     register long r3 __asm__("r3") = arg4;
-    register long r4 __asm__("r4") = arg5;
     register long r12 __asm__("r12") = number;
 
     __asm__ volatile (
-        "push {r4, r5, r6, r7}\n\t" 
+        "push {%[a5]}\n\t"
         "svc #0x80\n\t"
+        "pop {r1}\n\t"
         "mov %[err], #0\n\t"
         "movcs %[err], #1\n\t"
-	"pop {r4, r5, r6, r7}\n\t"
         : "+r" (r0), [err] "=r" (error_flag)
-        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4)
-        : "memory", "cc", "r5", "r6", "r7"
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), [a5] "r" (arg5)
+        : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
+FORCE_ARM
 long syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6)
 {
     long error_flag;
@@ -211,69 +198,50 @@ long syscall6(long number, long arg1, long arg2, long arg3, long arg4, long arg5
     register long r1 __asm__("r1") = arg2;
     register long r2 __asm__("r2") = arg3;
     register long r3 __asm__("r3") = arg4;
-    register long r4 __asm__("r4") = arg5;
-    register long r5 __asm__("r5") = arg6;
     register long r12 __asm__("r12") = number;
 
     __asm__ volatile (
-        "push {r4, r5, r6, r7}\n\t"
+        "push {%[a6]}\n\t"
+        "push {%[a5]}\n\t"
         "svc #0x80\n\t"
+        "pop {r1, r2}\n\t"
         "mov %[err], #0\n\t"
         "movcs %[err], #1\n\t"
-	"pop {r4, r5, r6, r7}\n\t"
         : "+r" (r0), [err] "=r" (error_flag)
-        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5)
-        : "memory", "cc", "r6", "r7"
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12),
+          [a5] "r" (arg5), [a6] "r" (arg6)
+        : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
-/* we need syscall8() for mmap */
-long syscall8(long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7, long arg8)
+FORCE_ARM
+long syscall8(long number, long arg1, long arg2, long arg3, long arg4,
+              long arg5, long arg6, long arg7, long arg8)
 {
     long error_flag;
     register long r0 __asm__("r0") = arg1;
     register long r1 __asm__("r1") = arg2;
     register long r2 __asm__("r2") = arg3;
     register long r3 __asm__("r3") = arg4;
-    register long r4 __asm__("r4") = arg5;
-    register long r5 __asm__("r5") = arg6;
-    register long r6 __asm__("r6") = arg7;
-    register long r7 __asm__("r7") = arg8;
     register long r12 __asm__("r12") = number;
 
     __asm__ volatile (
-        "push {r4, r5, r6, r7}\n\t"
+        "push {%[a8]}\n\t"
+        "push {%[a7]}\n\t"
+        "push {%[a6]}\n\t"
+        "push {%[a5]}\n\t"
         "svc #0x80\n\t"
+        "pop {r1, r2, r3, r12}\n\t"
         "mov %[err], #0\n\t"
         "movcs %[err], #1\n\t"
-	"pop {r4, r5, r6, r7}\n\t"
         : "+r" (r0), [err] "=r" (error_flag)
-        : "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5), "r" (r6), "r" (r7)
+        : "r" (r1), "r" (r2), "r" (r3), "r" (r12),
+          [a5] "r" (arg5), [a6] "r" (arg6), [a7] "r" (arg7), [a8] "r" (arg8)
         : "memory", "cc"
     );
-
-    if (error_flag)
-    {
-        errno = (int)r0;
-        return -1;
-    }
-
-    return r0;
+    RETURN_RESULT
 }
 
 # endif
-# ifdef __linux__
-#  error Linux not supported now
-# endif
-
-#else
-# error Architecture not supported
 #endif
