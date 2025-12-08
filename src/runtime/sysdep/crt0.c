f62107d912daab1d3c5cd8bb82ae6d953badd63f
Polina <aalx2176@gmail.com>
2025-12-08 13:27:21 +0300

    crt0

--- src/runtime/sysdep/crt0.c
+++ src/runtime/sysdep/crt0.c
@@ -1,34 +1,54 @@
-#include <stddef.h>
+#include <stdlib.h>
 
-int           NXArgc = 0;
-char**        NXArgv = NULL;
-char**        environ = NULL;
-char*         __progname = NULL;
+// Объявляем глобальные переменные (стандартные для окружения Apple/BSD)
+int NXArgc;
+char **NXArgv;
+char **environ;
+char *__progname;
 
-__asm__(
-    ".desc _NXArgc, 0x10\n"
-    ".desc _NXArgv, 0x10\n"
-    ".desc _environ, 0x10\n"
-    ".desc ___progname, 0x10"
-);
+// Объявляем main, чтобы компилятор знал о нем
+extern int main(int argc, char **argv, char **envp);
 
+/*
+ * Вспомогательная функция на C.
+ * Сюда мы попадаем из asm. Тут проще и нагляднее заполнить переменные.
+ */
+void _c_startup(int argc, char **argv, char **envp) {
+    NXArgc = argc;
+    NXArgv = argv;
+    environ = envp;
+    
+    // Обычно __progname — это argv[0] (имя запущенного файла)
+    __progname = (argv && argv[0]) ? argv[0] : "";
+
+    // Запускаем main и передаем результат в exit
+    exit(main(argc, argv, envp));
+}
+
+/*
+ * Точка входа (Entry Point).
+ * Задача: достать аргументы со стека, выровнять стек и вызвать C-код.
+ */
 __asm__(
     ".text\n"
     ".arm\n"
     ".align 2\n"
     ".globl start\n"
     "start:\n"
-    "    ldr r0, [sp]\n"
-    "    add r1, sp, #4\n"
-    "    add r12, r0, #1\n"
-    "    add r2, r1, r12, lsl #2\n"
-    "    mov r3, r2\n"
-    "1:\n"
-    "    ldr r12, [r3], #4\n"
-    "    cmp r12, #0\n"
-    "    bne 1b\n"
-    "    bic sp, sp, #7\n"
-    "    bl _main\n"
-    "    bl _exit\n"
-    "    trap\n"
+    // Стек при старте: [argc, argv[0], ..., NULL, envp[0], ...]
+    
+    "    ldr r0, [sp]            @ r0 = argc\n"
+    "    add r1, sp, #4          @ r1 = argv (адрес начала массива аргументов)\n"
+    
+    // Вычисляем адрес envp: argv + (argc + 1) * 4
+    "    add r2, r0, #1          @ r2 = argc + 1\n"
+    "    add r2, r1, r2, lsl #2  @ r2 = r1 + (r2 * 4) -> теперь в r2 лежит envp\n"
+    
+    "    bic sp, sp, #7          @ Выравниваем стек по 8 байт (требование ARM ABI)\n"
+    
+    // Вызываем нашу C-функцию. 
+    // На Mac/iOS компилятор добавляет нижнее подчеркивание к именам C-функций.
+    "    bl __c_startup\n" 
+    
+    "    trap                    @ Сюда попасть не должны, так как exit не возвращает управление\n"
 );
