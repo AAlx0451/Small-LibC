ca4cbb261cf04dda8d5582420db1446184e6eeeb
Polina <aalx2176@gmail.com>
2025-12-09 18:34:47 +0300

    time funcs, rand()

---
+++ src/sysindep/stdlib/rand.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+
+static unsigned long int next_seed = 1;
+
+void srand(unsigned int seed) {
+    next_seed = (seed == 0) ? 1 : seed;
+}
+
+int rand(void) {
+    const long a = 16807;
+    const long m = 2147483647; // 0x7fffffff
+    next_seed = (next_seed * a) % m;
+    return (int)next_seed;
+}
