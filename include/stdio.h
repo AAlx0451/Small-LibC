3eee408095dd4155781214a2bc902ad524fc951b
Polina <aalx2176@gmail.com>
2025-12-09 13:39:50 +0300

    time (?)

--- include/stdio.h
+++ include/stdio.h
@@ -4,4 +4,6 @@
 # define EOF -1
 #endif /* EOF */
 
+int puts(const char *s);
+
 /* STDIO_H */
---
+++ src/sysindep/time/time.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <sys/time.h>
+
+time_t time(time_t *tloc)
+{
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) < 0) {
+        return (time_t)-1;
+    }
+    if (tloc != NULL) {
+        *tloc = tv.tv_sec;
+    }
+    return tv.tv_sec;
+}
