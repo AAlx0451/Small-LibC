1b43d3d6b2fc7d8e81f4e59f841599ad5894d5b8
Polina <aalx2176@gmail.com>
2025-12-09 10:37:18 +0300

    crt CRITICAL bug fix

--- src/sysdep/syscall.c
+++ src/sysdep/syscall.c
@@ -4,7 +4,7 @@
  * are safer for clang
  * (add/str causes stack corruption)
  * be sure, that's the best way
- * to implement syscall(). I tried
+ * to implement syscall(). I tried.
  */
 
 #include <stdarg.h>
@@ -21,8 +21,8 @@
 
 #define REG(name, val) register long name __asm__(#name) = val
 
-#define _SYSCALL_GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, N, ...) N
-#define _SYSCALL_COUNT_ARGS(...) _SYSCALL_GET_NTH_ARG(0, ##__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
+#define _SYSCALL_GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, N, ...) N
+#define _SYSCALL_COUNT_ARGS(...) _SYSCALL_GET_NTH_ARG(0, ##__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #define _SYSCALL_CONCAT_IMPL(name, count) name##count
 #define _SYSCALL_CONCAT(name, count) _SYSCALL_CONCAT_IMPL(name, count)
 
@@ -107,6 +107,27 @@ DEFINE_SYSCALL(6, (long number, long arg1, long arg2, long arg3, long arg4, long
     "pop {r4}\n\t"
     "pop {r5}\n\t",
     "r" (r1), "r" (r2), "r" (r3), "r" (r12), [a5] "r" (arg5), [a6] "r" (arg6)
+)
+
+DEFINE_SYSCALL(7, (long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7),
+    REG(r0, arg1); REG(r1, arg2); REG(r2, arg3); REG(r3, arg4); REG(r12, number);,
+    "push {r6}\n\t"
+    "push {r5}\n\t"
+    "push {r4}\n\t"
+    "mov r6, %[a7]\n\t"
+    "mov r5, %[a6]\n\t"
+    "mov r4, %[a5]\n\t"
+    "push {r6}\n\t"
+    "push {r5}\n\t"
+    "push {r4}\n\t"
+    "svc #0x80\n\t"
+    "pop {r4}\n\t"
+    "pop {r5}\n\t"
+    "pop {r6}\n\t"
+    "pop {r4}\n\t"
+    "pop {r5}\n\t"
+    "pop {r6}\n\t",
+    "r" (r1), "r" (r2), "r" (r3), "r" (r12), [a5] "r" (arg5), [a6] "r" (arg6), [a7] "r" (arg7)
 )
 
 DEFINE_SYSCALL(8, (long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7, long arg8),
