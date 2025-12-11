1017e44db8cefe3c819795b0327949944f927dc2
Polina <aalx2176@gmail.com>
2025-12-11 14:28:31 +0300

    libm

--- include/math.h
+++ include/math.h
@@ -3,6 +3,7 @@
 
 #define NAN __builtin_nanf("0x7fc00000")
 #define HUGE_VAL __builtin_huge_val()
+#define INFINITY __builtin_huge_valf()
 
 #include <stdint.h>
 
@@ -19,28 +20,36 @@
 
 #define signbit(x) (__builtin_signbit(x))
 
-double fabs(double x);
-double floor(double x);
-long double floorl(long double x);
-double ceil(double x);
-double sqrt(double x);
-double fmod(double x, double y);
-double exp(double x);
-double log(double x);
-double pow(double x, double y);
-double frexp(double value, int *exp);
-double ldexp(double x, int exp);
-double modf(double value, double *iptr);
-double log10(double x);
-double sin(double x);
-double cos(double x);
-double tan(double x);
-double atan2(double y, double x);
-double atan(double x);
-double asin(double x);
 double acos(double x);
-double sinh(double x);
-double cosh(double x);
-double tanh(double x);
+double asin(double x);
+double atan(double x);
+double atan2(double y, double x);
+double ceil(double x);
+double copysign(double x, double y);
+double cos(double x);
+double exp(double x);
+double fabs(double x);
+double fdim(double x, double y);
+double floor(double x);
+double fmax(double x, double y);
+double fmin(double x, double y);
+double fmod(double x, double y);
+double frexp(double x, int *exp);
+double j0(double x);
+double j1(double x);
+double jn(int n, double x);
+double ldexp(double x, int exp);
+double log(double x);
+double log10(double x);
+double log2(double x);
+long   lround(double x);
+double modf(double x, double *iptr);
+double pow(double x, double y);
+double remainder(double x, double y);
+double round(double x);
+double sin(double x);
+double sqrt(double x);
+double tan(double x);
+double trunc(double x);
 
 #endif
