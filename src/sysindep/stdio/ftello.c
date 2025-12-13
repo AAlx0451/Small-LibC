63501f6aac1dc52c1c100c0e1ce750a12925dac1
Polina <aalx2176@gmail.com>
2025-12-13 17:08:17 +0300

    fread, fseek(o), ftell(o), fwrite bugfix, rewind

---
+++ src/sysindep/stdio/ftello.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+off_t ftello(FILE *stream) {
+    off_t pos;
+    _spin_lock(&stream->_lock);
+    
+    // For an accurate position, flush write buffers
+    if (stream->_flags & __S_WR) {
+        if (__stdio_flush_impl(stream) != 0) {
+            pos = (off_t)-1;
+            goto cleanup;
+        }
+    }
+    
+    pos = lseek(stream->_fd, 0, SEEK_CUR);
+    if (pos == (off_t)-1) {
+        goto cleanup;
+    }
+    
+    // If in read mode, the logical position is the physical position
+    // minus the number of bytes remaining in the buffer.
+    if (stream->_flags & __S_RD) {
+        pos -= stream->_cnt;
+    }
+
+cleanup:
+    _spin_unlock(&stream->_lock);
+    return pos;
+}
---
+++ src/sysindep/stdio/rewind.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void rewind(FILE *stream) {
+    (void)fseeko(stream, 0L, SEEK_SET);
+    _spin_lock(&stream->_lock);
+    stream->_flags &= ~__S_ERR; 
+    _spin_unlock(&stream->_lock);
+}
