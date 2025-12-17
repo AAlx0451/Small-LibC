0c7a337339dc1d4cafefe1bab5db6e6bc47ebc2a
Polina <aalx2176@gmail.com>
2025-12-17 10:36:29 +0300

    putc, mkstemp

---
+++ src/sysindep/stdlib/mkstemp.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/syscall.h>
+
+#define NUM_RETRIES 100
+#define XX_LEN 6
+
+int mkstemp(char *template)
+{
+    static int seeded = 0;
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    size_t len;
+    char *xx;
+    int fd;
+    int i;
+    int try_count;
+
+    if (!seeded) {
+        srand(time(NULL));
+        seeded = 1;
+    }
+
+    if (template == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    len = strlen(template);
+    if (len < XX_LEN) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    xx = template + len - XX_LEN;
+    for (i = 0; i < XX_LEN; ++i) {
+        if (xx[i] != 'X') {
+            errno = EINVAL;
+            return -1;
+        }
+    }
+
+    for (try_count = 0; try_count < NUM_RETRIES; ++try_count) {
+        for (i = 0; i < XX_LEN; ++i) {
+            xx[i] = charset[rand() % (sizeof(charset) - 1)];
+        }
+        
+        fd = syscall(SYS_open, (long)template, O_RDWR | O_CREAT | O_EXCL, 0600);
+
+        if (fd >= 0) {
+            return fd;
+        }
+        
+        if (errno != EEXIST) {
+            return -1;
+        }
+    }
+
+    errno = EEXIST;
+    return -1;
+}
