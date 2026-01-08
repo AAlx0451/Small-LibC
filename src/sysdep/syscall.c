fce56042f6d865539bb675a88e43cc0e699025c9
Polina <aalx2176@gmail.com>
2026-01-08 21:40:36 +0300

    syscall: updated for safety

--- src/sysdep/syscall.c
+++ src/sysdep/syscall.c
@@ -8,6 +8,29 @@
 #ifdef __arm__
 # ifdef __APPLE__
 
+/*
+ * simple helper
+ * aligns stack by 16 bytes (pushing 4 regs) and marks stack changes for CFI
+ */
+__asm__ ( // static __attribute__("naked") void do_syscall(void)
+    ".text\n\t"
+    ".align 4\n\t"
+    ".thumb_func _do_syscall\n\t"
+    "_do_syscall:\n\t"
+    ".cfi_startproc\n\t"
+    "push {r4, r5, r6, r8}\n\t"
+    ".cfi_def_cfa_offset 16\n\t"
+    ".cfi_offset r4, -16\n\t"
+    ".cfi_offset r5, -12\n\t"
+    ".cfi_offset r6, -8\n\t"
+    ".cfi_offset r8, -4\n\t"
+    "svc #128\n\t"
+    "pop {r4, r5, r6, r8}\n\t"
+    ".cfi_def_cfa_offset 0\n\t"
+    "bx lr\n\t"
+    ".cfi_endproc\n"
+);
+
 #define REG(name, val) register long name __asm__(#name) = (long)(val)
 
 #define _SYSCALL_GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, N, ...) N
@@ -19,7 +42,8 @@
     _SYSCALL_CONCAT(syscall, _SYSCALL_COUNT_ARGS(__VA_ARGS__))(number, ##__VA_ARGS__)
 
 // we don't add r4-r6,r8 to clobbers, because we bind
-// args to register and compiler follows ABI
+// args to register and compiler follows ABI and will generate correct prologue 
+// "lr" in clobbers because we now use 'bl' instruction for >4 args
 #define DEFINE_SYSCALL(n, args, regs, asm_code, ...) \
 long syscall##n args \
 { \
@@ -29,11 +53,11 @@ long syscall##n args \
     __asm__ volatile ( \
         asm_code \
         "mov %[err], #0\n\t" \
-        "it cs\n\t" /* Thumb-2 requirement for conditional execution */ \
+        "it cs\n\t" /* thumb requirement for conditional execution, works for UAL ARM as well */ \
         "movcs %[err], #1\n\t" \
         : "+r" (r0), [err] "=r" (error_flag) \
         : __VA_ARGS__ \
-        : "memory", "cc" \
+        : "memory", "cc", "lr", "sp" \
     ); \
     \
     if (error_flag) { \
@@ -73,39 +97,34 @@ DEFINE_SYSCALL(4, (long number, long arg1, long arg2, long arg3, long arg4),
     "r" (r1), "r" (r2), "r" (r3), "r" (r12)
 )
 
+// for 5+ args we use _do_syscall
+// we always fill r4, r5, r6, r8 to for alignment
+
 DEFINE_SYSCALL(5, (long number, long arg1, long arg2, long arg3, long arg4, long arg5),
     REG(r0, arg1); REG(r1, arg2); REG(r2, arg3); REG(r3, arg4); REG(r12, number);
-    REG(r4, arg5); REG(r5, 0);,
-    "push {r4, r5}\n\t" // r5 is padding
-    "svc #0x80\n\t"
-    "pop {r4, r5}\n\t",
-    "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5)
+    REG(r4, arg5); REG(r5, 0); REG(r6, 0); REG(r8, 0);, // r5, r6, r8 are for padding
+    "bl _do_syscall\n\t",
+    "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5), "r" (r6), "r" (r8)
 )
 
 DEFINE_SYSCALL(6, (long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6),
     REG(r0, arg1); REG(r1, arg2); REG(r2, arg3); REG(r3, arg4); REG(r12, number);
-    REG(r4, arg5); REG(r5, arg6);,
-    "push {r4, r5}\n\t"
-    "svc #0x80\n\t"
-    "pop {r4, r5}\n\t",
-    "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5)
+    REG(r4, arg5); REG(r5, arg6); REG(r6, 0); REG(r8, 0);, // r6, r8 are for padding
+    "bl _do_syscall\n\t",
+    "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5), "r" (r6), "r" (r8)
 )
 
 DEFINE_SYSCALL(7, (long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7),
     REG(r0, arg1); REG(r1, arg2); REG(r2, arg3); REG(r3, arg4); REG(r12, number);
-    REG(r4, arg5); REG(r5, arg6); REG(r6, arg7); REG(r8, 0);,
-    "push {r4, r5, r6, r8}\n\t" // r8 is padding
-    "svc #0x80\n\t"
-    "pop {r4, r5, r6, r8}\n\t",
+    REG(r4, arg5); REG(r5, arg6); REG(r6, arg7); REG(r8, 0);, // r8 is for stack padding
+    "bl _do_syscall\n\t",
     "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5), "r" (r6), "r" (r8)
 )
 
 DEFINE_SYSCALL(8, (long number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long arg7, long arg8),
     REG(r0, arg1); REG(r1, arg2); REG(r2, arg3); REG(r3, arg4); REG(r12, number);
     REG(r4, arg5); REG(r5, arg6); REG(r6, arg7); REG(r8, arg8);,
-    "push {r4, r5, r6, r8}\n\t"
-    "svc #0x80\n\t"
-    "pop {r4, r5, r6, r8}\n\t",
+    "bl _do_syscall\n\t",
     "r" (r1), "r" (r2), "r" (r3), "r" (r12), "r" (r4), "r" (r5), "r" (r6), "r" (r8)
 )
 
