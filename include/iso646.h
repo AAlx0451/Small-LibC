57801fac25f3c284bf672df2cd560971b49a03d4
Polina <aalx2176@gmail.com>
2026-01-16 20:44:28 +0300

    iso646.h

---
+++ include/iso646.h
@@ -0,0 +1,18 @@
+#ifndef ISO646_H
+#define ISO646_H
+
+#ifndef __cplusplus // to suppress errrors
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+#endif
+
+#endif // ISO646_H
