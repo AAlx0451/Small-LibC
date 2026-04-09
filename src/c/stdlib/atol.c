62787b0a8818e01252d866e4303e8c9aa2ede7e9
Polina <aalx2176@gmail.com>
2026-04-09 21:33:41 +0300

    refactor: removed repeatative atol/atoi code in favor of strtol

--- src/c/stdlib/atol.c
+++ src/c/stdlib/atol.c
@@ -2,20 +2,5 @@
 #include <stdlib.h>
 
 long atol(const char *nptr) {
-    long result = 0;
-    long sign = 1;
-    while(isspace((unsigned char)*nptr)) {
-        nptr++;
-    }
-    if(*nptr == '-') {
-        sign = -1;
-        nptr++;
-    } else if(*nptr == '+') {
-        nptr++;
-    }
-    while(isdigit((unsigned char)*nptr)) {
-        result = result * 10 + (*nptr - '0');
-        nptr++;
-    }
-    return result * sign;
+    return (int)strtol(nptr, (char **)NULL, 10);
 }
