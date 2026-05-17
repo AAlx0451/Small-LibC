1e4ab338408b595bf468a7801d09d956d0f4f088
Polina Ovsyannikova <aalx2176@gmail.com>
2026-05-18 00:12:42 +0300

    feat: trunc implementation

---
+++ src/math/trunc.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+double trunc(double x) {
+    uint64_t i;
+    int exponent;
+    uint64_t mask;
+
+    memcpy(&i, &x, 8);
+    exponent = (int)((i >> 52) & 0x7FF) - 1023;
+
+    if (exponent < 0) {
+        i &= 0x8000000000000000ULL;
+    } else if (exponent < 52) {
+        mask = ~((1ULL << (52 - exponent)) - 1);
+        i &= mask;
+    }
+
+    memcpy(&x, &i, 8);
+    return x;
+}
