421d98df87f8653f2c769e9a8e81cf64bf1fb5e4
Polina Ovsyannikova <aalx2176@gmail.com>
2026-05-22 00:09:31 +0300

    feat: implement std_types.h

---
+++ include/mach/std_types.h
@@ -0,0 +1,16 @@
+#ifndef MACH_STD_TYPES_H
+#define MACH_STD_TYPES_H
+
+#include <features.h>
+
+#if defined(_MACH_SOURCE)
+
+#include <stdint.h>
+#include <mach/boolean.h>
+#include <mach/kern_return.h>
+#include <mach/port.h>
+#include <mach/vm_types.h>
+
+#endif /* !_MACH_SOURCE */
+
+#endif  /* !MACH_STD_TYPES_H */
