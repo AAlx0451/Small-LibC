c27417cc4aaf280eb037ff0e3282c4b8b9a63383
Polina Ovsyannikova <aalx2176@gmail.com>
2026-05-26 08:35:05 +0300

    refactor(stdint.h): make more readable

--- include/stdint.h
+++ include/stdint.h
@@ -3,193 +3,129 @@
 
 #include <features.h>
 
-#if !defined(_ANSI) && ((defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L) || defined(_DARWIN_C_SOURCE))
+#if !defined(_ANSI) &&                                                                             \
+    ((defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) ||                                 \
+     (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L) || defined(_DARWIN_C_SOURCE))
 
-#define __WORDSIZE 32
+# define __WORDSIZE 32
 
-#ifndef _INT8_T
-#define _INT8_T
-typedef signed char           int8_t;
-#endif /*_INT8_T */
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
 
-#ifndef _INT16_T
-#define _INT16_T
-typedef short                int16_t;
-#endif /* _INT16_T */
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 
-#ifndef _INT32_T
-#define _INT32_T
-typedef int                  int32_t;
-#endif /* _INT32_T */
+typedef int8_t int_least8_t;
+typedef int16_t int_least16_t;
+typedef int32_t int_least32_t;
+typedef int64_t int_least64_t;
+typedef uint8_t uint_least8_t;
+typedef uint16_t uint_least16_t;
+typedef uint32_t uint_least32_t;
+typedef uint64_t uint_least64_t;
 
-#ifndef _INT64_T
-#define _INT64_T
-typedef long long            int64_t;
-#endif /* _INT64_T */
+typedef int8_t int_fast8_t;
+typedef int16_t int_fast16_t;
+typedef int32_t int_fast32_t;
+typedef int64_t int_fast64_t;
+typedef uint8_t uint_fast8_t;
+typedef uint16_t uint_fast16_t;
+typedef uint32_t uint_fast32_t;
+typedef uint64_t uint_fast64_t;
 
-#ifndef _UINT8_T
-#define _UINT8_T
-typedef unsigned char         uint8_t;
-#endif /*_UINT8_T */
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
 
-#ifndef _UINT16_T
-#define _UINT16_T
-typedef unsigned short       uint16_t;
-#endif /* _UINT16_T */
+typedef __INTMAX_TYPE__ intmax_t;
+typedef __UINTMAX_TYPE__ uintmax_t;
 
-#ifndef _UINT32_T
-#define _UINT32_T
-typedef unsigned int         uint32_t;
-#endif /* _UINT32_T */
+# define INT8_MAX  127
+# define INT16_MAX 32767
+# define INT32_MAX 2147483647
+# define INT64_MAX 9223372036854775807LL
 
-#ifndef _UINT64_T
-#define _UINT64_T
-typedef unsigned long long   uint64_t;
-#endif /* _UINT64_T */
+# define INT8_MIN  -128
+# define INT16_MIN -32768
+# define INT32_MIN -2147483648
+# define INT64_MIN -9223372036854775808LL
 
-typedef int8_t           int_least8_t;
-typedef int16_t         int_least16_t;
-typedef int32_t         int_least32_t;
-typedef int64_t         int_least64_t;
-typedef uint8_t         uint_least8_t;
-typedef uint16_t       uint_least16_t;
-typedef uint32_t       uint_least32_t;
-typedef uint64_t       uint_least64_t;
+# define UINT8_MAX  255
+# define UINT16_MAX 65535
+# define UINT32_MAX 4294967295U
+# define UINT64_MAX 18446744073709551615ULL
 
-typedef int8_t            int_fast8_t;
-typedef int16_t          int_fast16_t;
-typedef int32_t          int_fast32_t;
-typedef int64_t          int_fast64_t;
-typedef uint8_t          uint_fast8_t;
-typedef uint16_t        uint_fast16_t;
-typedef uint32_t        uint_fast32_t;
-typedef uint64_t        uint_fast64_t;
+# define INT_LEAST8_MIN  INT8_MIN
+# define INT_LEAST16_MIN INT16_MIN
+# define INT_LEAST32_MIN INT32_MIN
+# define INT_LEAST64_MIN INT64_MIN
 
-#ifndef _INTPTR_T
-#define _INTPTR_T
-typedef long   intptr_t;
-#endif /* _INTPTR_T */
+# define INT_LEAST8_MAX  INT8_MAX
+# define INT_LEAST16_MAX INT16_MAX
+# define INT_LEAST32_MAX INT32_MAX
+# define INT_LEAST64_MAX INT64_MAX
 
-#ifndef _UINTPTR_T
-#define _UINTPTR_T
-typedef unsigned long   uintptr_t;
-#endif /* _UINTPTR_T */
+# define UINT_LEAST8_MAX  UINT8_MAX
+# define UINT_LEAST16_MAX UINT16_MAX
+# define UINT_LEAST32_MAX UINT32_MAX
+# define UINT_LEAST64_MAX UINT64_MAX
 
-#ifndef _INTMAX_T
-#define _INTMAX_T
-#ifdef __INTMAX_TYPE__
-typedef __INTMAX_TYPE__             intmax_t;
-#else /* __INTMAX_TYPE__ */
-typedef long long                intmax_t;
-#endif /* __INTMAX_TYPE__ */
-#endif /* _INTMAX_T */
+# define INT_FAST8_MIN  INT8_MIN
+# define INT_FAST16_MIN INT16_MIN
+# define INT_FAST32_MIN INT32_MIN
+# define INT_FAST64_MIN INT64_MIN
 
-#ifndef _UINTMAX_T
-#define _UINTMAX_T
-#ifdef __UINTMAX_TYPE__
-typedef __UINTMAX_TYPE__             uintmax_t;
-#else /* __UINTMAX_TYPE__ */
-typedef unsigned long long      uintmax_t;
-#endif /* __UINTMAX_TYPE__ */
-#endif /* _UINTMAX_T */
+# define INT_FAST8_MAX  INT8_MAX
+# define INT_FAST16_MAX INT16_MAX
+# define INT_FAST32_MAX INT32_MAX
+# define INT_FAST64_MAX INT64_MAX
 
+# define UINT_FAST8_MAX  UINT8_MAX
+# define UINT_FAST16_MAX UINT16_MAX
+# define UINT_FAST32_MAX UINT32_MAX
+# define UINT_FAST64_MAX UINT64_MAX
 
-/* LIMITS */
-#define INT8_MAX         127
-#define INT16_MAX        32767
-#define INT32_MAX        2147483647
-#define INT64_MAX        9223372036854775807LL
+# define INTPTR_MIN INT32_MIN
+# define INTPTR_MAX INT32_MAX
 
-#define INT8_MIN          -128
-#define INT16_MIN         -32768
-#define INT32_MIN        (-INT32_MAX-1)
-#define INT64_MIN        (-INT64_MAX-1)
+# define UINTPTR_MAX UINT32_MAX
 
-#define UINT8_MAX         255
-#define UINT16_MAX        65535
-#define UINT32_MAX        4294967295U
-#define UINT64_MAX        18446744073709551615ULL
+# define INTMAX_MIN INT64_MIN
+# define INTMAX_MAX INT64_MAX
 
-#define INT_LEAST8_MIN    INT8_MIN
-#define INT_LEAST16_MIN   INT16_MIN
-#define INT_LEAST32_MIN   INT32_MIN
-#define INT_LEAST64_MIN   INT64_MIN
+# define UINTMAX_MAX UINT64_MAX
 
-#define INT_LEAST8_MAX    INT8_MAX
-#define INT_LEAST16_MAX   INT16_MAX
-#define INT_LEAST32_MAX   INT32_MAX
-#define INT_LEAST64_MAX   INT64_MAX
+# define PTRDIFF_MIN INT32_MIN
+# define PTRDIFF_MAX INT32_MAX
 
-#define UINT_LEAST8_MAX   UINT8_MAX
-#define UINT_LEAST16_MAX  UINT16_MAX
-#define UINT_LEAST32_MAX  UINT32_MAX
-#define UINT_LEAST64_MAX  UINT64_MAX
+# define SIZE_MAX UINT32_MAX
 
-#define INT_FAST8_MIN     INT8_MIN
-#define INT_FAST16_MIN    INT16_MIN
-#define INT_FAST32_MIN    INT32_MIN
-#define INT_FAST64_MIN    INT64_MIN
+# define WCHAR_MAX __WCHAR_MAX__
+# define WCHAR_MIN (-WCHAR_MAX - 1)
 
-#define INT_FAST8_MAX     INT8_MAX
-#define INT_FAST16_MAX    INT16_MAX
-#define INT_FAST32_MAX    INT32_MAX
-#define INT_FAST64_MAX    INT64_MAX
+# define WINT_MIN INT32_MIN
+# define WINT_MAX INT32_MAX
 
-#define UINT_FAST8_MAX    UINT8_MAX
-#define UINT_FAST16_MAX   UINT16_MAX
-#define UINT_FAST32_MAX   UINT32_MAX
-#define UINT_FAST64_MAX   UINT64_MAX
+# define SIG_ATOMIC_MIN INT32_MIN
+# define SIG_ATOMIC_MAX INT32_MAX
 
-#define INTPTR_MIN        INT32_MIN
-#define INTPTR_MAX        INT32_MAX
+# define INT8_C(v)  (v)
+# define INT16_C(v) (v)
+# define INT32_C(v) (v)
+# define INT64_C(v) (v##LL)
 
-#define UINTPTR_MAX       UINT32_MAX
+# define UINT8_C(v)  (v##U)
+# define UINT16_C(v) (v##U)
+# define UINT32_C(v) (v##U)
+# define UINT64_C(v) (v##ULL)
 
-#define INTMAX_MIN        INT64_MIN
-#define INTMAX_MAX        INT64_MAX
+# define INTMAX_C(v)  (v##LL)
+# define UINTMAX_C(v) (v##ULL)
 
-#define UINTMAX_MAX       UINT64_MAX
-
-#define PTRDIFF_MIN       INT32_MIN
-#define PTRDIFF_MAX       INT32_MAX
-
-#define SIZE_MAX          UINT32_MAX
-
-#ifndef WCHAR_MAX
-#  ifdef __WCHAR_MAX__
-#    define WCHAR_MAX     __WCHAR_MAX__
-#  else
-#    define WCHAR_MAX     0x7fffffff
-#  endif
-#endif
-
-#ifndef WCHAR_MIN
-#  if WCHAR_MAX == 0xffff
-#    define WCHAR_MIN       0
-#  else
-#    define WCHAR_MIN       (-WCHAR_MAX-1)
-#  endif
-#endif
-
-#define WINT_MIN	  INT32_MIN
-#define WINT_MAX	  INT32_MAX
-
-#define SIG_ATOMIC_MIN	  INT32_MIN
-#define SIG_ATOMIC_MAX	  INT32_MAX
-
-#define INT8_C(v)    (v)
-#define INT16_C(v)   (v)
-#define INT32_C(v)   (v)
-#define INT64_C(v)   (v ## LL)
-
-#define UINT8_C(v)   (v ## U)
-#define UINT16_C(v)  (v ## U)
-#define UINT32_C(v)  (v ## U)
-#define UINT64_C(v)  (v ## ULL)
-
-#define INTMAX_C(v)  (v ## LL)
-#define UINTMAX_C(v) (v ## ULL)
-
-#endif /* !_ANSI && ((__STDC_VERSION__ && __STDC_VERSION__ >= 199901L) || (_POSIX_C_SOURCE && _POSIX_C_SOURCE >= 200112L) || _DARWIN_C_SOURCE) */
+#endif /* !_ANSI && ((__STDC_VERSION__ && __STDC_VERSION__ >= 199901L) || (_POSIX_C_SOURCE &&      \
+          _POSIX_C_SOURCE >= 200112L) || _DARWIN_C_SOURCE) */
 
 #endif /* !STDINT_H */
