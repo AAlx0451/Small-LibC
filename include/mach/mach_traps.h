ec73c4aad5d50ff753b70ed0594e60d01f4ec58f
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-09 18:59:08 +0300

    feat(mach_traps.h): add missing declararions based on mach_types.h

--- include/mach/mach_traps.h
+++ include/mach/mach_traps.h
@@ -3,21 +3,11 @@
 
 #include <features.h>
 
-#define __TODO 0
-
 #if defined(_MACH_SOURCE)
 
+# include <mach/mach_types.h>
 # include <mach/std_types.h>
 # include <stdint.h>
-# if __TODO
-#  include <mach/clock_types.h>
-#  include <mach/mach_types.h>
-#  include <machine/endian.h>
-# endif /* __TODO */
-# include <mach/kern_return.h>
-# include <mach/message.h>
-# include <mach/port.h>
-# include <mach/vm_types.h>
 
 # if defined(_C_SOURCE)
 
@@ -57,7 +47,6 @@ extern kern_return_t semaphore_wait_trap(mach_port_name_t wait_name);
 extern kern_return_t semaphore_wait_signal_trap(mach_port_name_t wait_name,
                                                 mach_port_name_t signal_name);
 
-#  if __TODO
 extern kern_return_t semaphore_timedwait_trap(mach_port_name_t wait_name,
                                               unsigned int sec,
                                               clock_res_t nsec);
@@ -72,7 +61,6 @@ extern kern_return_t clock_sleep_trap(mach_port_name_t clock_name,
                                       int sleep_sec,
                                       int sleep_nsec,
                                       mach_timespec_t *wakeup_time);
-#  endif /* __TODO */
 
 extern kern_return_t _kernelrpc_mach_vm_allocate_trap(mach_port_name_t target,
                                                       mach_vm_offset_t *addr,
@@ -83,13 +71,13 @@ extern kern_return_t _kernelrpc_mach_vm_deallocate_trap(mach_port_name_t target,
                                                         mach_vm_address_t address,
                                                         mach_vm_size_t size);
 
-#  if __TODO
+#  if 0
 extern kern_return_t _kernelrpc_mach_vm_protect_trap(mach_port_name_t target,
                                                      mach_vm_address_t address,
                                                      mach_vm_size_t size,
                                                      boolean_t set_maximum,
                                                      vm_prot_t new_protection);
-#  endif /* __TODO */
+#  endif /* 0 */
 
 extern kern_return_t _kernelrpc_mach_port_allocate_trap(mach_port_name_t target,
                                                         mach_port_right_t right,
@@ -110,12 +98,10 @@ extern kern_return_t _kernelrpc_mach_port_move_member_trap(mach_port_name_t targ
                                                            mach_port_name_t member,
                                                            mach_port_name_t after);
 
-#  if __TODO
 extern kern_return_t _kernelrpc_mach_port_insert_right_trap(mach_port_name_t target,
                                                             mach_port_name_t name,
                                                             mach_port_name_t poly,
                                                             mach_msg_type_name_t polyPoly);
-#  endif /* __TODO */
 
 extern kern_return_t _kernelrpc_mach_port_insert_member_trap(mach_port_name_t target,
                                                              mach_port_name_t name,
@@ -139,11 +125,9 @@ extern boolean_t swtch_pri(int pri);
 
 extern boolean_t swtch(void);
 
-#  if __TODO
 extern kern_return_t thread_switch(mach_port_name_t thread_name,
                                    int option,
                                    mach_msg_timeout_t option_time);
-#  endif /* __TODO */
 
 extern mach_port_name_t task_self_trap(void);
 
