891e24c5a8a42bdec607f6b762c30ac4695087f6
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-09 19:49:19 +0300

    feat(sched.h): add sched_param definition

--- include/sched.h
+++ include/sched.h
@@ -1,6 +1,22 @@
-#ifndef SCHED_H
+#ifndef SCHED_
 #define SCHED_H
 
+#include <features.h>
+
+#if !defined(_ANSI) && (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 199309L)
+
+struct sched_param
+{
+    int sched_priority;
+#if defined(_DARWIN_C_SOURCE)
+    int quantum;
+#else
+    char opaque[sizeof(int)];
+#endif /* _DARWIN_C_SOURCE */
+};
+
 int sched_yield(void);
+
+#endif /* !_ANSI && (_POSIX_C_SOURCE && _POSIX_C_SOURCE >= 199309L) */
 
 #endif /* !SCHED_H */
