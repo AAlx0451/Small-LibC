398be47d2cc34c417c84584c1bf9cceb184e982d
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-09 19:59:20 +0300

    fix(sched.h): include guard typo

--- include/sched.h
+++ include/sched.h
@@ -1,18 +1,17 @@
-#ifndef SCHED_
+#ifndef SCHED_H
 #define SCHED_H
 
 #include <features.h>
 
 #if !defined(_ANSI) && (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 199309L)
 
-struct sched_param
-{
+struct sched_param {
     int sched_priority;
-#if defined(_DARWIN_C_SOURCE)
+# if defined(_DARWIN_C_SOURCE)
     int quantum;
-#else
+# else
     char opaque[sizeof(int)];
-#endif /* _DARWIN_C_SOURCE */
+# endif /* _DARWIN_C_SOURCE */
 };
 
 int sched_yield(void);
