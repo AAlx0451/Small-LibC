1872289d1f48273b42c514c766796c6abf95444c
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-15 19:17:28 +0300

    feat: add mach.h umbrella header

---
+++ include/mach/mach.h
@@ -0,0 +1,10 @@
+#ifndef MACH_MACH_H
+#define MACH_MACH_H
+
+// IWYU pragma: begin_exports
+#include <mach/mach_syscalls.h>
+#include <mach/semaphore.h>
+#include <mach/vm_param.h>
+// IWYU pragma: end_exports
+
+#endif /* !MACH_MACH_H */
