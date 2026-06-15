2621e2e2b72c77a4f3fb4b740b25dbfb14cd75a1
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-15 23:08:00 +0300

    feat: add <mach/host_notify.h>

--- include/mach/mach_types.h
+++ include/mach/mach_types.h
@@ -4,8 +4,9 @@
 
 #ifndef MACH_MACH_TYPES_H
 
-# include <mach/host_info.h>
 # include <mach/clock_types.h>
+# include <mach/host_info.h>
+# include <mach/host_notify.h>
 # include <mach/machine.h>
 # include <mach/machine/vm_types.h>
 # include <mach/message.h>
