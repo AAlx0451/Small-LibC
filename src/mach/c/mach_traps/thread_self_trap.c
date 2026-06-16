e3cb491bafc85597b69b42eb24b5e2e5316f7fff
Polina Ovsyannikova <aalx2176@gmail.com>
2026-06-16 16:52:08 +0300

    chore(mach_traps): add explicit mach_port_name_t casts

--- src/mach/c/mach_traps/thread_self_trap.c
+++ src/mach/c/mach_traps/thread_self_trap.c
@@ -1,4 +1,7 @@
 #include <mach/mach_syscalls.h>
 #include <mach/mach_traps.h>
 
-mach_port_name_t thread_self_trap(void) { return mach_syscall(SYS_thread_self_trap); }
+mach_port_name_t thread_self_trap(void)
+{
+    return (mach_port_name_t)mach_syscall(SYS_thread_self_trap);
+}
